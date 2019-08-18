3edf6368acfd74d6f45d1617775554da5a4ba7f3
vladislavvovk <vladislav.vovk@nextiva.com>
2019-08-18 22:05:43 +0300

    Fixed snackbar

--- client/src/modules/AlertTreshold/components/AlertTreshold.js
+++ client/src/modules/AlertTreshold/components/AlertTreshold.js
@@ -7,23 +7,26 @@ import styles from './AlertTreshold.module.scss';
 
 const AlertTreshold = ({ data }) => {
   const [treshold, setTreshold] = useState(0);
-  const [numberAboveTreshold, setNumberAboveTreshold] = useState(null);
+  const [numberAboveTreshold, setNumberAboveTreshold] = useState(0);
+  const [ showSnack, setShowSnack ] = useState(false);
+  
   
   const lastIncomingValue = data.length && data[ data.length - 1 ].value;
   
   if (lastIncomingValue && lastIncomingValue > parseInt(treshold) && lastIncomingValue !== numberAboveTreshold) {
     setNumberAboveTreshold(lastIncomingValue);
+    setShowSnack(true);
   }
   
   return (
     <div className={styles.container}>
       <span className={styles.info}>Enter a treshold value, and a snackbar will be shown if received value is greater than treshold</span>
       <input type="number" value={treshold} onChange={(event) => setTreshold(event.target.value)}/>
-      {numberAboveTreshold && (
+      {showSnack && (
         <Snackbar
           message={`Received number above treshold: ${numberAboveTreshold}`}
           timeoutMs={4000}
-          onClose={() => setNumberAboveTreshold(null)}
+          onClose={() => setShowSnack(false)}
         />
       )}
     </div>
